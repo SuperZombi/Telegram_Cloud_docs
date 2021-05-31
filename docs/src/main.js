58ce6ff911cb98498d8f03fcdac7ef61d58549c7
Super Zombi <rost201541@gmail.com>
2021-05-31 19:53:04 +0300

    Update main.js

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,9 +1,9 @@
-var player1 = new Playerjs({id:"player1", file:"docs/Authorization.mp4"});
-var player2 = new Playerjs({id:"player2", file:"docs/Upload_your_files.mp4"});
-var player3 = new Playerjs({id:"player3", file:"docs/Работа с папками.mp4"});
-var player4 = new Playerjs({id:"player4", file:"docs/Переименование и перемещение.mp4"});
-var player5 = new Playerjs({id:"player5", file:"docs/Скачивание_удаление.mp4"});
-
+var player1 = new Playerjs({id:"player1", file:"docs/Installation.mp4"});
+var player2 = new Playerjs({id:"player2", file:"docs/Authorization.mp4"});
+var player3 = new Playerjs({id:"player3", file:"docs/Upload_your_files.mp4"});
+var player4 = new Playerjs({id:"player4", file:"docs/Работа с папками.mp4"});
+var player5 = new Playerjs({id:"player5", file:"docs/Переименование и перемещение.mp4"});
+var player6 = new Playerjs({id:"player6", file:"docs/Скачивание_удаление.mp4"});
 
 // Canvas
 var c=document.getElementById("nightsky");
